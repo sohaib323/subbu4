5a549bb24d7a3b21f5571f1c406ad2db7c137f0f
sohaib323 <skhan121999@gmail.com>
2025-05-25 19:26:45 +0530

    Add files via upload

---
+++ subbu.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("We will talk about indias states")
+st.write("we have 30 states in the india!")
+
+st.markdown("<h1 style='color:red;'>This is Red</h1>", unsafe_allow_html=True)
+st.button('click here to open link')
